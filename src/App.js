792b177bfc4a95a4235e3260ef8ced11a64d2558
Umair Arshad <Umair6622@gmail.com>
2021-04-09 20:44:58 +0500

    First react component with NO JXS

---
+++ src/App.js
@@ -0,0 +1,37 @@
+let startAt = performance.now();
+
+const Pet = ({name,animal,bread}) => {
+    return React.createElement("div",{},[
+        React.createElement("h1",{},name),
+        React.createElement("h2",{},animal),
+        React.createElement("h2",{},bread)
+    ]);
+};
+
+const App = () => {
+    return React.createElement(
+      "div",
+      {},
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, {
+          name:"Luna", 
+          animal:"Dod", 
+          bread:"Russian"
+        }),
+      React.createElement(Pet, {
+          name:"Luna", 
+          animal:"Dod", 
+          bread:"Russian"
+        }),
+      React.createElement(Pet, {
+          name:"Luna", 
+          animal:"Dod", 
+          bread:"Russian"
+        })
+    );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+let endAt = performance.now();
+console.log(endAt - startAt)
