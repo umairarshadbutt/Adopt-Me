13d686af34f6a8dafe6c1f4a062e11db32cc4755
Umair Arshad <Umair6622@gmail.com>
2021-04-14 01:13:18 +0500

    extract pet component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from "react";
 import {render} from "react-dom";
-
+import Pet from "./Pet";
 let startAt = performance.now();
 
-const Pet = ({name,animal,bread}) => {
-    return React.createElement("div",{},[
-        React.createElement("h1",{},name),
-        React.createElement("h2",{},animal),
-        React.createElement("h2",{},bread)
-    ]);
-};
+
 
 const App = () => {
     return React.createElement(
