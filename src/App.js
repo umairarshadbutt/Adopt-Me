026a63a24e5ca5478c877d03478cf8b0b5cd9433
Umair Arshad <Umair6622@gmail.com>
2021-04-14 03:14:01 +0500

    Expressions

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
 import React from "react";
 import {render} from "react-dom";
 import Pet from "./Pet";
-let startAt = performance.now();
-
-
-
 const App = () => {
-    return React.createElement(
-      "div",
-      {},
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-          name:"Luna", 
-          animal:"Dod", 
-          bread:"Russian"
-        }),
-      React.createElement(Pet, {
-          name:"Luna", 
-          animal:"Dod", 
-          bread:"Russian"
-        }),
-      React.createElement(Pet, {
-          name:"Luna", 
-          animal:"Dod", 
-          bread:"Russian"
-        })
+    // return React.createElement(
+    //   "div",
+    //   {},
+    //   React.createElement("h1", {}, "Adopt Me!"),
+    //   React.createElement(Pet, {
+    //       name:"Luna", 
+    //       animal:"Dod", 
+    //       bread:"Russian"
+    //     }),
+    //   React.createElement(Pet, {
+    //       name:"Luna", 
+    //       animal:"Dod", 
+    //       bread:"Russian"
+    //     }),
+    //   React.createElement(Pet, {
+    //       name:"Luna", 
+    //       animal:"Dod", 
+    //       bread:"Russian"
+    //     })
+    // );
+
+    return (
+      <div>
+        <h1 id="something-important">
+          Adopt Me!
+        </h1>
+        <Pet name="Lucky" animal="Dog" bread="Bull"/>
+        <Pet name="Meni" animal="Cat" bread="Mixed"/>
+      </div>
     );
 };
 
 render(React.createElement(App), document.getElementById("root"));
-
-let endAt = performance.now();
-console.log(endAt - startAt)
